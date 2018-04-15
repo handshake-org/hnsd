5426fac6929d11b37598efe8cba625bcad4645a7
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-04-14 17:39:12 -0700

    src: add error.c.

---
+++ src/error.c
@@ -0,0 +1,51 @@
+#include "config.h"
+
+#include <assert.h>
+#include <stdint.h>
+
+#include "error.h"
+
+static const char *errstrs[] = {
+  "ESUCCESS",
+  "ENOMEM",
+  "ETIMEOUT",
+  "EFAILURE",
+  "EBADARGS",
+  "EENCODING",
+  "EHASHMISMATCH",
+  "EMALFORMEDNODE",
+  "EINVALIDNODE",
+  "EEARLYEND",
+  "ENORESULT",
+  "EUNKNOWNERROR",
+  "EUNEXPECTEDNODE",
+  "ERECORDMISMATCH",
+  "EPOWPROOFSIZE",
+  "EPOWTOOBIG",
+  "EPOWTOOSMALL",
+  "EPOWNONMATCHING",
+  "EPOWBRANCH",
+  "EPOWDEADEND",
+  "EPOWSHORTCYCLE",
+  "ENEGTARGET",
+  "EHIGHHASH",
+  "ETIMETOONEW",
+  "EDUPLICATE",
+  "EDIPLICATEORPHAN",
+  "ETIMETOOOLD",
+  "EBADDDIFFBITS",
+  "EORPHAN",
+  "EACTONE",
+  "EACTTWO",
+  "EACTTHREE",
+  "EBADSIZE",
+  "EBADTAG",
+  "EUNKNOWN"
+};
+
+const char *
+hsk_strerror(int32_t code) {
+  if (code > HSK_MAXERROR)
+    return errstrs[HSK_MAXERROR + 1];
+  return errstrs[code];
+}
