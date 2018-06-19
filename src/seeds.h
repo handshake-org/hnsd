94e49ab143302e5ed067c64d2e7f81c30b0ab2fc
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-06-19 10:04:18 -0700

    seeds: add default seeds.

--- src/seeds.h
+++ src/seeds.h
@@ -17,6 +17,10 @@ static const char *hsk_seeds[] = {
  */
 
 static const char *hsk_seeds[] = {
+  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@173.255.248.12",
+  "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@66.175.217.103",
+  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@45.56.92.136",
+  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@45.56.82.169",
   NULL
 };
 
