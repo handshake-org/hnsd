9698d54c47672e04b2beebdf90d4f860e84eb7b6
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-08-02 01:58:36 -0700

    seeds: fix comma.

--- src/seeds.h
+++ src/seeds.h
@@ -20,7 +20,7 @@ static const char *hsk_seeds[] = {
   "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",
   "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126",
   "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",
-  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168"
+  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168",
   NULL
 };
 
