handshake-org/hnsd

commit 7d82c2140864a10ae69467c92420d328c55a511b

pool: ignore duplicate or otherwise bad seeds during daemon init

src/pool.c
@@ -263,17 +263,23 @@ hsk_pool_set_seeds(hsk_pool_t *pool, const char *seeds) {
         continue;
       }
 
-      if (size >= HSK_MAX_HOST)
-        return false;
+      if (size >= HSK_MAX_HOST) {
+        hsk_pool_log(pool, "seed address exceeds maximum length allowed.\n");
+        continue;
+      }
 
       memcpy(&seed[0], &seeds[start], size);
       seed[size] = '\0';
 
-      if (!hsk_addr_from_string(&addr, seed, 0))
-        return false;
+      if (!hsk_addr_from_string(&addr, seed, 0)) {
+        hsk_pool_log(pool, "could not parse seed from string: %s\n", seed);
+        continue;
+      }
 
-      if (!hsk_addrman_add_addr(&pool->am, &addr))
-        return false;
+      if (!hsk_addrman_add_addr(&pool->am, &addr)) {
+        hsk_pool_log(pool, "could not add seed: %s\n", seed);
+        continue;
+      }
 
       start = i + 1;
     }

commit 60a3796e4bb3b976c4d56d8abb1dbd332fec9c67

pool: ignore duplicate block errors from chain
Fixes a regression caused by 7d82c2140864a10ae69467c92420d328c55a511b
that would destory peers that sent duplicate blocks (which is very
common thing for multiple peers to do!)

src/pool.c
@@ -1449,7 +1449,10 @@ hsk_peer_handle_headers(hsk_peer_t *peer, const hsk_headers_msg_t *msg) {
 
     if (rc != HSK_SUCCESS) {
       hsk_peer_log(peer, "failed adding block: %s\n", hsk_strerror(rc));
-      return rc;
+      if (rc == HSK_EDUPLICATE)
+        return HSK_SUCCESS;
+      else
+        return rc;
     }
 
     peer->headers += 1;