f0f19b7060bf0cb30e5291ebc802463f77db6815
Matthew Zipkin <pinheadmz@gmail.com>
2021-12-03 11:04:58 -0500

    seeds: update mainnet brontide seeds

--- src/seeds.h
+++ src/seeds.h
@@ -7,13 +7,17 @@
  */
 
 static const char *hsk_seeds[] = {
-  "aonetsezqp4m52w4jpfq2gv3dggy2wqfwqtkfjyttgdidbvhgp5as@165.22.151.242",
-  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",
-  "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126",
-  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",
-  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168",
-  "akimcha5bck7s344dmge6k3agtxd2txi6x4qzg3mo26spvf5bjol2@74.207.247.120",
-  "ap5vuwabzwyz6akhesanada4skhetd2jsvpkwuqxzuaoovn5ez4xg@45.79.134.225",
+  "aonetsezqp4m52w4jpfq2gv3dggy2wqfwqtkfjyttgdidbvhgp5as@165.22.151.242",   // pinheadmz
+  "ajpbuxkwtf7hkwbpw27siwg6dylsly4743rbhy2jssb3rxmircbrs@45.79.216.182",    // Falci
+  "aplie5zadqqlghqvbq7by4i4ce7rngenwyxhmiq4byzv6svnqd5b2@138.197.52.107",   // rithvikvibhu
+  "ai2wxnj4magzrhtlfvqnzuzg3vko25vryle5vd2bzi23jvcykbj5q@107.152.33.71",    // buffrr
+  "apt4rf2dfyelbivg63u47wykvdjtsl4kxzfdylkaae5s5ydldlnwu@159.69.46.23",     // anon
+  "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126",  // chjj
+  "akimcha5bck7s344dmge6k3agtxd2txi6x4qzg3mo26spvf5bjol2@74.207.247.120",   // chjj
+  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",  // chjj
+  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",  // chjj
+  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168",  // chjj
+  "ap5vuwabzwyz6akhesanada4skhetd2jsvpkwuqxzuaoovn5ez4xg@45.79.134.225",    // chjj
   NULL
 };
 
