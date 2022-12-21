cbffd72608adf82b1ca52fce18cd5ab295319b51
Matthew Zipkin <pinheadmz@gmail.com>
2022-12-21 13:41:54 -0500

    seeds: update for 2.x

--- src/seeds.h
+++ src/seeds.h
@@ -7,17 +7,74 @@
  */
 
 static const char *hsk_seeds[] = {
-  "aonetsezqp4m52w4jpfq2gv3dggy2wqfwqtkfjyttgdidbvhgp5as@165.22.151.242",   // pinheadmz
-  "ajpbuxkwtf7hkwbpw27siwg6dylsly4743rbhy2jssb3rxmircbrs@45.79.216.182",    // Falci
-  "aplie5zadqqlghqvbq7by4i4ce7rngenwyxhmiq4byzv6svnqd5b2@138.197.52.107",   // rithvikvibhu
-  "ai2wxnj4magzrhtlfvqnzuzg3vko25vryle5vd2bzi23jvcykbj5q@107.152.33.71",    // buffrr
-  "apt4rf2dfyelbivg63u47wykvdjtsl4kxzfdylkaae5s5ydldlnwu@159.69.46.23",     // anon
-  "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126",  // chjj
-  "akimcha5bck7s344dmge6k3agtxd2txi6x4qzg3mo26spvf5bjol2@74.207.247.120",   // chjj
-  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",  // chjj
-  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",  // chjj
-  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168",  // chjj
-  "ap5vuwabzwyz6akhesanada4skhetd2jsvpkwuqxzuaoovn5ez4xg@45.79.134.225",    // chjj
+  // pinheadmz
+  "aonetsezqp4m52w4jpfq2gv3dggy2wqfwqtkfjyttgdidbvhgp5as@165.22.151.242",
+  // Falci
+  "ajpbuxkwtf7hkwbpw27siwg6dylsly4743rbhy2jssb3rxmircbrs@50.116.35.178",
+  // rithvikvibhu
+  "aksygghkgmciomeldjf5sc6rs2sgn2m34zfdz4xr7z5vguqvjis4e@129.153.177.220",
+  // buffrr
+  "ai2wxnj4magzrhtlfvqnzuzg3vko25vryle5vd2bzi23jvcykbj5q@107.152.33.71",
+  // anon
+  "apt4rf2dfyelbivg63u47wykvdjtsl4kxzfdylkaae5s5ydldlnwu@159.69.46.23",
+  // chjj
+  "ajdzrpoxsusaw4ixq4ttibxxsuh5fkkduc5qszyboidif2z25i362@173.255.209.126",
+  // chjj
+  "akimcha5bck7s344dmge6k3agtxd2txi6x4qzg3mo26spvf5bjol2@74.207.247.120",
+  // chjj
+  "aoihqqagbhzz6wxg43itefqvmgda4uwtky362p22kbimcyg5fdp54@172.104.214.189",
+  // chjj
+  "ajk57wutnhfdzvqwqrgab3wwh4wxoqgnkz4avbln54pgj5jwefcts@172.104.177.177",
+  // chjj
+  "am2lsmbzzxncaptqjo22jay3mztfwl33bxhkp7icfx7kmi5rvjaic@139.162.183.168",
+  // chjj
+  "ap5vuwabzwyz6akhesanada4skhetd2jsvpkwuqxzuaoovn5ez4xg@45.79.134.225",
+  // Other nodes discovered by seed.easyhandshake.com and htools-org/hnsnodes
+  "104.254.246.153",
+  "66.94.118.127",
+  "165.227.93.117",
+  "98.47.90.111",
+  "85.214.33.200",
+  "178.18.254.92",
+  "152.69.186.119",
+  "89.58.17.86",
+  "76.217.158.15",
+  "149.102.152.193",
+  "139.59.211.187",
+  "147.124.228.198",
+  "108.175.4.26",
+  "66.29.151.42",
+  "205.250.112.133",
+  "44.229.138.206",
+  "81.6.58.121",
+  "154.26.133.109",
+  "140.238.164.155",
+  "81.169.159.30",
+  "50.112.123.184",
+  "185.232.71.108",
+  "207.244.233.76",
+  "140.238.196.136",
+  "193.41.237.153",
+  "82.223.165.215",
+  "212.227.68.123",
+  "202.61.201.15",
+  "168.138.31.136",
+  "107.152.32.250",
+  "108.61.206.38",
+  "204.99.129.8",
+  "73.185.40.66",
+  "129.153.166.244",
+  "137.184.156.222",
+  "5.161.64.49",
+  "176.96.136.143",
+  "150.136.231.120",
+  "54.184.104.94",
+  "47.108.194.84",
+  "45.79.95.228",
+  "89.163.154.217",
+  "139.178.67.226",
+  "47.242.86.29",
+  "99.92.204.3",
   NULL
 };
 
